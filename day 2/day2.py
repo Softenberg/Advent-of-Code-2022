c35ea8832238c935f04e0e210b7b376572c6d614
Andreas <93anan@gmail.com>
2022-12-02 15:03:24 +0100

    B I G

---
+++ day 2/day2.py
@@ -0,0 +1,40 @@
+f = open("day 2/day2.txt", "r")
+score = 0
+ABC=["A", "B", "C"]
+XYZ=["X", "Y", "Z"]
+
+for line in f:
+    opp = line[0]
+    outcome = line[2]
+    opp=ABC.index(opp)
+    
+    if outcome == "Y":
+        me = opp
+    elif outcome == "Z":
+        me = opp + 1
+        if me == 3:
+            me =0
+
+    else:
+        me = opp - 1
+        if me == -1:
+            me =2
+    
+        
+    if me == opp + 1 or (me ==0 and opp ==2):
+        score += 6
+    elif me == opp:
+        score += 3
+
+    score = score + me + 1
+
+print(score)    
+
+# A = rock
+# B = paper
+# C = Scissors
+
+# X = Win
+# Y = Draw
+# Z = Loss
+
