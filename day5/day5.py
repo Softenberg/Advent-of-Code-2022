3e5830099460667237a0cdecf10c6ab911284be4
Andreas <93anan@gmail.com>
2022-12-05 15:19:26 +0100

    day5

---
+++ day5/day5.py
@@ -0,0 +1,49 @@
+with open("day5/day5.txt", "r") as file:
+    lines = [line.rstrip() for line in file]
+
+listslist=[]
+for i in range(9):
+    templist =[]
+    listslist.append(templist)    
+
+for line in lines:
+    for i in range(1,40,2): 
+        try:
+            if line[i] == " ":
+                continue
+            else:
+                if i == 1:
+                    listslist[0].append(line[i])
+                else:
+                    listslist[round((i-1)/4)].append(line[i])    
+        except IndexError:
+            continue
+    
+    if line[-1] == "9":
+        break
+
+for i in listslist:
+    i.reverse()
+
+
+for line in lines:
+    if line[0] == "m":
+        #print(line)
+        
+        amount=int(line[5:7])
+        fro = int(line[12:14])-1
+        to = int(line [17:19])-1
+        print(amount, fro, to)
+        print(listslist)
+        templist = listslist[fro][-amount:len(listslist[fro])]
+        listslist[fro]=listslist[fro][0:-amount]
+        print(templist)
+        for i in templist:
+            listslist[to].append(i)
+    
+final_string=""    
+for i in listslist:
+    final_string+=i.pop(-1)
+    
+print(final_string)
+    
---
+++ day5/test.py
@@ -0,0 +1,5 @@
+li = [0, 1, 2, 3, 4, 5, 6, 7]
+
+temp_li = li[-3:len(li)]
+
+print(temp_li)
